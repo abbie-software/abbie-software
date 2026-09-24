1aa6acf72551cd0408a06791ad8f21abf0336d7c
Abigail Gathoni <abigail.murigi@students.jkuat.ac.ke>
2026-09-24 17:11:58 +0300

    wired the intro loader into the page.tsx so that i can see if it works according to how i would want it

--- abbieverse/app/page.tsx
+++ abbieverse/app/page.tsx
@@ -1,9 +1,21 @@
-import {ThemeToggle } from "@/src/components/theme-toggle";
+"use client";
+
+import { useState } from "react";
+import IntroLoader from "@/src/components/intro-loader";
+import { ThemeToggle } from "@/src/components/theme-toggle";
+
 export default function Home() {
+  const [introDone, setIntroDone] = useState(false);
+
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3x1">Launching Abbieverse</h1>
-      <ThemeToggle />
-      </main>
+    <>
+      {!introDone && <IntroLoader onComplete={() => setIntroDone(true)} />}
+      {introDone && (
+        <main className="flex min-h-screen flex-col items-center justify-center gap-6">
+          <h1 className="text-3xl">Main site coming next </h1>
+          <ThemeToggle />
+        </main>
+      )}
+    </>
   );
 }
